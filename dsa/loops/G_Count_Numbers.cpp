9a4309c994d11fdf3ba65793037dbf247daa0589
Nitish C <86357181+Nitish-bot@users.noreply.github.com>
2026-01-25 20:55:56 +0530

    Progress on loops

--- dsa/loops/G_Count_Numbers.cpp
+++ dsa/loops/G_Count_Numbers.cpp
@@ -5,5 +5,26 @@ int main() {
   int a;
   cin >> a;
 
-  
+  int input;
+  int pos = 0, neg = 0, eve = 0, odd = 0;
+  for (int i = 0; i < a; i++) {
+    cin >> input;
+
+    if (input % 2 == 0) {
+      eve += 1;
+    } else {
+      odd += 1;
+    }
+
+    if (input > 0) {
+      pos += 1;
+    } else if (input < 0) {
+      neg += 1;
+    }
+  }
+
+  cout << pos << endl;
+  cout << neg << endl;
+  cout << eve << endl;
+  cout << odd << endl;
 }
